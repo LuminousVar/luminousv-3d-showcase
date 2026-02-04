a73beb404d90db956805f187b93bb8d47a8ae8ab
LuminousV <farelreyhan6@gmail.com>
2026-02-04 13:11:08 +0700

    feat: implement camera perspective

---
+++ src/lib/Experience/Camera.ts
@@ -0,0 +1,22 @@
+import { PerspectiveCamera } from 'three';
+import type Experience from './Experience';
+import { CAMERA_POSITION } from './constants';
+
+export default class Camera {
+	private experience: Experience;
+	instance: PerspectiveCamera;
+
+	constructor(experience: Experience) {
+		this.experience = experience;
+		this.instance = new PerspectiveCamera(
+			20,
+			this.experience.config.width / this.experience.config.height,
+			0.1,
+			1000
+		);
+
+		this.instance.rotation.reorder('XYZ');
+		this.instance.rotation.copy(CAMERA_POSITION);
+		this.experience.scene.add(this.instance);
+	}
+}
---
+++ src/lib/Experience/constants.ts
@@ -0,0 +1,11 @@
+import { Vector2, Vector3, Color, Quaternion, PlaneGeometry, Euler } from 'three';
+
+// Camera
+export const CAMERA_POSITION = new Vector3(-23, 17, 23);
+export const CAMERA_QUATERNION = new Quaternion(
+	-0.19229498096591757,
+	-0.3743024144764491,
+	-0.07965118909235921,
+	0.9036459654580388
+);
+export const CAMERA_TARGET = new Vector3(0, 2.5, 0);
